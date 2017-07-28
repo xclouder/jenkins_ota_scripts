95f666cd1fbd302e9340459c2372beb975ea2a5d
xClouder <xclouder@qq.com>
2017-07-28 12:28:39 +0800

    bugfix

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,6 +1,6 @@
 #环境变量
 WORKSPACE=`pwd`
-BUILD_URL="http://ci.ezfun.cn/otatest/15"
+BUILD_URL="http://ci.ezfun.cn/otatest/15/"
 echo "BUILD_DIR :$BUILD_DIR"
 
 BUILD_DIR="build"
@@ -13,4 +13,4 @@ mkdir -p build
 #Simulate build
 cp OTATest.ipa $BUILD_DIR/OTATest.ipa
 
-sh ./ota.sh ${WORKSPACE}/${BUILD_DIR} ${BUILD_URL}/$BUILD_DIR/ $OTA_WEB_URL
+sh ./ota.sh ${WORKSPACE}/${BUILD_DIR} ${BUILD_URL}artifact/$BUILD_DIR/ $OTA_WEB_URL
